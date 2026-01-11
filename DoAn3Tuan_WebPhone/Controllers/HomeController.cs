3598012140e0826dbe1835d287c30ecf053b8c6e
bingCr2005 <hongky1910@gmail.com>
2026-01-11 17:37:48 +0700

    Init ASP.NET Core MVC project

---
+++ DoAn3Tuan_WebPhone/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using DoAn3Tuan_WebPhone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAn3Tuan_WebPhone.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DoAn3Tuan_WebPhone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DoAn3Tuan_WebPhone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
