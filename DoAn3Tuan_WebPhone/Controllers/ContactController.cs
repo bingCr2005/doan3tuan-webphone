ac1174ac451465a58cdd5970cc4a33f9a3dec284
bingCr2005 <hongky1910@gmail.com>
2026-01-18 23:31:39 +0700

    update ContactController, View, css cho trang liên hệ và cập nhật menu dieu huong: Su dung Tag Helpers thay cho link tĩnh

---
+++ DoAn3Tuan_WebPhone/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoAn3Tuan_WebPhone.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // Hàm xử lý gửi form (POST)
+        [HttpPost]
+        public IActionResult GuiTin(string HoTen, string Email, string SoDienThoai, string NoiDung)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
