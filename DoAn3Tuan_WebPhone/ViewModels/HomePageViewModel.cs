a6b101a8764841041add90da1378436582a71a55
bingCr2005 <hongky1910@gmail.com>
2026-01-20 18:09:01 +0700

    khoi tao HomePageViewModel va them anh quang cao iPhone 17prm cho slideshow

---
+++ DoAn3Tuan_WebPhone/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using DoAn3Tuan_WebPhone.Models;
+namespace DoAn3Tuan_WebPhone.ViewModels
+{
+    public class HomePageViewModel
+    {
+        //Sản phẩm nổi bật hoặc đang giảm giá mạnh để chạy Slideshow
+        public List<DienThoai> SanPhamNoiBat { get; set; } = new();
+
+        //Danh sách các điện thoại mới nhập về
+        public List<DienThoai> SanPhamMoi { get; set; } = new();
+
+        //Danh sách điện thoại có số lượng bán ra nhiều nhất
+        public List<DienThoai> SanPhamBanChay { get; set; } = new();
+
+        // Danh sách các hãng hiện ở Sidebar và Brand Slider
+        public List<HangDienThoai> DanhSachHang { get; set; } = new();
+    }
+}
